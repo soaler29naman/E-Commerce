ccf3f779706be326a83458babcd9d24ead639ca1
Naman Maurya <namanmaurya000@gmail.com>
2024-01-18 10:08:57 +0530

    Seconddayupdatation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 
 function App() {
@@ -16,9 +20,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
@@ -30,7 +34,7 @@ function App() {
         
       </Routes>
       
-      
+      <Footer/>
       </BrowserRouter>
 
     </div>
